c79ecaea95998fe9cccadda72f2e25d50c207145
jayashri <jaysharipatil01@gmail.com>
2025-07-09 00:12:28 +0530

    init

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse
+
+
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+     return render(
+        request,
+        template_name="website/index.html"
+    
+     )
+   
+        
+
+        
+        
+
+            
+    
