e7416e152b3a993bd94838b6f9c2a482aa49a0b3
jayashri <jaysharipatil01@gmail.com>
2025-07-10 18:48:56 +0530

    inittt

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    blog_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+    
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Blog
 
 
 def index(request):
